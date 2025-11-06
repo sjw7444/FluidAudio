00a08c884a603560a1871672bbae8ce67fff8f3a
Brandon Weng <18161326+BrandonWeng@users.noreply.github.com>
2025-11-06 00:46:03 -0500

    MacOS catalyst support (#181)
    ### Why is this change needed?
    <!-- Explain the motivation for this change. What problem does it solve?
    -->

---
+++ Sources/FluidAudio/Frameworks/ESpeakNG.xcframework/ios-arm64_x86_64-maccatalyst/ESpeakNG.framework/Headers/ESpeakNG.h
@@ -0,0 +1,6 @@
+#import <ESpeakNG/espeak_ng.h>
+#import <ESpeakNG/speak_lib.h>
+#import <ESpeakNG/encoding.h>
+#import <ESpeakNG/espeak-ng/espeak_ng.h>
+#import <ESpeakNG/espeak-ng/speak_lib.h>
+#import <ESpeakNG/espeak-ng/encoding.h>
